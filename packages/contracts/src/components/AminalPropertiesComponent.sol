1486dccfb5af60b3e8b9b749dc822632260cf449
Will Papper <contact@papper.me>
2022-11-11 16:16:59 +0100

    Added stub of setters for structs

--- packages/contracts/src/components/AminalPropertiesComponent.sol
+++ packages/contracts/src/components/AminalPropertiesComponent.sol
@@ -7,16 +7,10 @@ uint256 constant ID = uint256(keccak256("component.AminalProperties"));
 contract AminalPropertiesComponent is Component {
   struct AminalStruct {
     address favorite;
-    address coordinates;
     uint256 totalFed;
     uint256 totalMoved;
-    // We can calculate hunger based on lastFed.
     uint256 lastFed;
-    // We can have a fun multiplier like "lastExercised" if we wanted to for
-    // the gotos. That would require them to state the max movement they
-    // want to occur and be refunded for any unpurchased movements.
     uint256 lastMoved;
-    uint256 lastPooped;
     // This data will not be updated after spawn
     uint256 spawnTime;
     address spawnedBy;
@@ -30,5 +24,17 @@ contract AminalPropertiesComponent is Component {
   // Mapping of Aminal IDs to Aminal structs
   mapping(uint256 => AminalStruct) aminalProperties;
 
-  constructor(address world) Component(world, ID) {}
+  constructor(address world) Component(world, ID) {
+    function setFed(address world, address fedBy, address fedAmount) {
+
+    }
+
+    function setMoved(address world, address movedBy, address movedAmount) {
+
+    }
+
+    function setSpawned(address world, address spawnedBy, address spawnedAt) {
+
+    }
+  }
 }
